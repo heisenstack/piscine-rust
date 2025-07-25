21f071041280d0f429c4de28a38c6c8003611b6c
mdinani <4minedina@gmail.com>
2025-07-25 13:40:22 +0100

    matrix_transposition

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -1,3 +1,8 @@
-pub fn transpose(m: Matrix) -> Matrix {
+ #[derive(Debug, PartialEq, Eq)]
 
+pub struct Matrix(pub(i32, i32),pub(i32, i32));
+
+pub fn transpose(m: Matrix) -> Matrix {
+    let Matrix((a,b),(c,d)) = m;
+    Matrix((a,c),(b,d))
 }
