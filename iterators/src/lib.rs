0bc6e217fd5c04becad42a64fff27f7f91325ee2
mdinani <4minedina@gmail.com>
2025-08-16 16:44:06 +0100

    iterators

---
+++ iterators/src/lib.rs
@@ -0,0 +1,38 @@
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Collatz {
+    pub v: u64,
+}
+
+impl Collatz {
+    pub fn new(n: u64) -> Self {
+        Collatz { v: n }
+    }
+}
+
+impl Iterator for Collatz {
+    type Item = Collatz;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.v == 0 || self.v == 1 {
+            return None;
+        }
+
+        let current = *self;
+
+        if self.v % 2 == 0 {
+            self.v /= 2;
+        } else {
+            self.v = 3 * self.v + 1;
+        }
+
+        Some(current)
+    }
+}
+
+pub fn collatz(n: u64) -> usize {
+    if n <= 1 {
+        0
+    } else {
+        Collatz::new(n).count()
+    }
+}
